c9cb29efd070a43226171029168cdafabf41676b
Kyle Berkof <32805176+BKB10@users.noreply.github.com>
2022-07-10 16:42:58 -0400

    Add project files.

---
+++ KSFP/Main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "SoundFontFile.h"
+
+int main() {
+	SoundFontFile* soundFont = new SoundFontFile("F:/Music/Soundfonts/FluidR3 GM Modified Just Piano.sf2"); //FluidR3 GM Modified Just Piano
+
+	return 0;
+}
